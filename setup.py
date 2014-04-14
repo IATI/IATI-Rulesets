56501953fb5353633d9fb8475bea237e59a55f86
Ben Webb <bjwebb67@googlemail.com>
2014-04-14 14:22:27 +0100

    [#12] Add rules_text function to this repository

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='iatirulesets',
       version='0.0.0',
       license='MIT',
       url='https://github.com/IATI/IATI-Rulesets',
-      py_modules=['iatirulesets'],
+      packages=['iatirulesets'],
       install_requires=[
         'lxml'
       ],
