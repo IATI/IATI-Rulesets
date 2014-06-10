7096ad3ac5830200fc801eaf63a1d6c6b582da22
Ben Webb <bjwebb67@googlemail.com>
2014-06-10 16:17:53 +0100

    Remove newlines from rulesets->text

--- iatirulesets/text.py
+++ iatirulesets/text.py
@@ -28,7 +28,7 @@ def rules_text(rules, reduced_path, show_all=False):
                             if other_paths:
                                 out.append('Either ``{0}`` or ``{1}`` must be present.'.format(case_path, human_list(other_paths)))
                             else:
-                                out.append('``{0}`` must be present.\n\n'.format(case_path))
+                                out.append('``{0}`` must be present.'.format(case_path))
                         elif rule == 'startswith':
                             out.append('``{0}`` should start with the value in ``{1}``'.format(case_path, case['start']))
                         elif rule == 'regex_matches':
@@ -37,11 +37,11 @@ def rules_text(rules, reduced_path, show_all=False):
             elif rule == 'date_order':
                 if show_all or simplify_xpath(case['less']) == reduced_path or simplify_xpath(case['more']) == reduced_path:
                     if case['less'] == 'NOW':
-                        out.append('``{0}`` must be in the future.\n\n'.format(case['more']))
+                        out.append('``{0}`` must be in the future.'.format(case['more']))
                     elif case['more'] == 'NOW':
-                        out.append('``{0}`` must be today, or in the past.\n\n'.format(case['less']))
+                        out.append('``{0}`` must be today, or in the past.'.format(case['less']))
                     else:
-                        out.append('``{0}`` must be before ``{1}``\n\n'.format(case['less'], case['more']))
+                        out.append('``{0}`` must be before ``{1}``'.format(case['less'], case['more']))
             else: print('Not implemented', case_path, rule, case['paths'])
     return out
 
