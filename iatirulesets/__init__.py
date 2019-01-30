440127f430d05b562c332cd0d3504d1f2b270473
Ocre42 <john.askew@devinit.org>
2019-01-30 15:50:29 +0000

    cleanup date_now function and meta test

--- iatirulesets/__init__.py
+++ iatirulesets/__init__.py
@@ -81,9 +81,12 @@ class Rules(object):
 
     def date_now(self, case):
         datetime_element = self.element.xpath(case['date'])
-        m1 = xsDateTimeRegex.match(datetime_element[0])
-        mapped_datetime = datetime.datetime(*map(int, m1.groups()))
-        return mapped_datetime < datetime.datetime.now()
+        if len(datetime_element) > 0:
+            m1 = xsDateTimeRegex.match(datetime_element[0])
+            mapped_datetime = datetime.datetime(*map(int, m1.groups()))
+            return mapped_datetime < datetime.datetime.now()
+        else:
+            return None
 
     def _regex_matches(self, case):
         return [ re.search(case['regex'], get_text(path_match)) for path_match in self.path_matches ]
