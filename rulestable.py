af200944935c897c93a3c2a5d276fca036294bca
Samuele Mattiuzzo <samumatt@gmail.com>
2019-03-11 15:51:24 +0000

    (2.01) Added range rule. Min/max are optional so we can check if the value is at least/no more as well #173

--- rulestable.py
+++ rulestable.py
@@ -18,9 +18,7 @@ print """
     - Element/Attribute
     - Requirement
     - Tested if
-
-"""
-                
+"""             
 
 english = {
     'date_order': 'Dates must be in correct order',
@@ -31,6 +29,7 @@ english = {
     'sum': 'Must sum to {0}',
     'startswith': 'Must start with ``{0}``',
     'unique': 'Unique',
+    'range': 'The value must be at least ``min`` and no more than ``max`` (inclusive).',
     'evaluates_to_true': '```eval``` must evaluate to true',
     'time_limit': 'Length must be under a year',
     'date_now': 'Date must not be more recent than the current date',
