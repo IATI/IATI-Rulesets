e174bf367e89a249a59c69c9475b292ee312a613
Rolf Kleef <rolf@drostan.org>
2019-04-14 17:37:52 +0200

    Remove JSON-based rulesets testing
    Also example.py and setup.py

--- example.py
+++
@@ -1,7 +0,0 @@
-import lxml.etree as ET
-import json, sys
-from iatirulesets import test_ruleset
-
-ruleset = json.load(open(sys.argv[1]))
-tree = ET.parse(sys.argv[2])
-print(test_ruleset(ruleset,tree))
