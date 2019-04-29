4ddd5eff71e376f1e5d0cad368a01aed51e6dc99
Samuele Mattiuzzo <samumatt@gmail.com>
2019-04-29 11:50:14 +0100

    Removes the no_percent rule #187

--- iatirulesets/text.py
+++ iatirulesets/text.py
@@ -63,8 +63,6 @@ def rules_text(rules, reduced_path, show_all=False):
                                 out.append('The sum of values matched at ``{0}`` must be ``{2}``.'.format(case_path, sum_total))
                         elif rule == 'strict_sum':
                             out.append('The sum of values matched at ``{0}`` must be ``{1}``.'.format(case_path, case['sum']))
-                        elif rule == 'no_percent':
-                            out.append('``{0}`` must not contain a ``%`` sign.'.format(case_path))
                         elif rule == 'range':
                             min_val = case.get('min')
                             max_val = case.get('max')
