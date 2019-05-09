b374859e83c9d2c78938ecf94d1a01fbb6c3c6fd
Francesca Sargent <francesca@fffunction.co>
2019-05-09 12:28:40 +0100

    Adding Python 3.x, amending linting and travis

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-setup(name='iatirulesets',
-      version='0.0.0',
-      license='MIT',
-      url='https://github.com/IATI/IATI-Rulesets',
-      packages=['iatirulesets'],
-      install_requires=[
+setup(
+    name='iatirulesets',
+    version='0.0.0',
+    license='MIT',
+    url='https://github.com/IATI/IATI-Rulesets',
+    packages=['iatirulesets'],
+    install_requires=[
         'lxml'
-      ],
-      )
+    ],
+)
