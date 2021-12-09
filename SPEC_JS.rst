2cd1a14f6c584000ae6d76e708c73ddcc12b6c40
Nik Osvalds <60047271+nosvalds@users.noreply.github.com>
2021-12-09 11:50:47 +0000

    Validator V2 Updates - v2.03 (#248)
    * prettier format
    
    * initial updates for validatorV2
    
    * Add 102.1.1
    
    * 1.1.1 rule added
    
    * fix location of 102.1.1
    
    * 1.1.3 reporting-org to iati-identifier check
    
    * 1.1.21 identifiers rule added
    
    * 12.3.1 sector % range
    
    * 2.1.2 sector percentage sums
    
    * 6.14.1 and 6.13.1 policy-marker attribute rules
    
    * 6.2.2 sector requirement
    
    * 6.7.2 sector
    
    * 6.6.2 rule
    
    * 7.8.1 rule added for currency
    
    * 107.1.1 default-aid-type
    
    * 12.1.1 recipient-region/country range
    
    * 12.2.1 capital-spend and budget-item range
    
    * 3.1.2 recipient-country percentage
    
    * 3.4.2 recipient-region/country percentage sum
    
    * organisation identifier prefix checks against ORG-ID
    
    * 7.5.3 budget period
    
    * 7.5.2 value-date check
    
    * 8.X.3 result indicator value omission
    
    * 8.6.3 period date_order
    
    * 1.X.1 no spaces or newlines rules
    
    * 1.X.13 regex_matches no special characters
    
    * adding ruleInfo to many existing rules
    
    * result indicator value number type check
    
    * 7.9.1 7.9.2 7.9.4 country budget item percentages
    
    * 107.1.2 default aid type limit
    
    * 107.2.1 complete
    
    * 107.2.2 complete
    
    * 2.1.1 sector percentage requirements
    
    * 2.1.4 sector vocab percentage requirements
    
    * refactor to use or inside [] for some xpaths
    
    * 4.3.1 4.4.1 title and description narrative checks
    
    * 3.6.2, 3.7.1, 3.7.2 recipient location rules
    
    * 3.4.1 complete
    
    * 3.1.4 3.4.4 added, various small fixes
    
    * 3.1.1 added
    
    * add  @vocabulary = '' as option
    
    * more specific name
    
    * add 7.8.2 currency check
    
    * remove 1.X.8 and 1.X.13 rules that aren't in V1
    
    * add idConditions to identifier checks
    
    * 3.4.2 add recipient-country into condition
    
    * remove 1yr budget limit for planned-disbursement 7.5.3, added in error
    
    * add paths to if_then rules for additional context generation in the output
    
    * fix message for 3.4.1
    
    * fix incorrectly placed rules in JSON
    
    * schema updates and push to redis workflow
    
    * add JS implementation specifications
    
    * remove 7.5.2 as it duplicates Schema check
    
    * add job to trigger csv update on rule tracker repo
    
    * fix spelling errors with identifier
    
    * remove duplicative rule 1.1.1 and update message for 1.1.21
    
    * fix 107.2.2 by moving to transaction level evaluation
    
    * add commit sha to redis cache
    
    * use file commit sha instead of current commit sha
    
    * try different format
    
    * add -- separator for filepath
    
    * add fetch-depth
    
    * add support for guidance links
    
    * push rulesets to PROD redis-cacher
    
    * updates for validator v2
    
    * remove reference to Readme.md
    
    * link and workflow information added
    
    * additional updates
    
    * add missing guidance links
    
    * 107.1.2 adding guidance link
    
    * apply 4.3.1 and 4.4.1 to all title and description narratives like V1
    
    * use specific paths to increase performance where possible
    
    * Simplify rules text generation
    
    * Rule links
    
    * Lint
    
    * Multiple possible locations to pull path from
    
    * Lint
    
    * Sort like version numbers
    
    * 2.1.2 only evaluate sum if more than one sector element is present per vocabulary
    
    * comment out PRD redis update while merging into version-2.03 branch
    
    Co-authored-by: akmiller01 <alex.k.miller@gmail.com>

---
+++ SPEC_JS.rst
@@ -0,0 +1,166 @@
+
+IATI Ruleset Spec - JS Edition
+=================
+
+An IATI Ruleset is a JSON document. The structure is described below. This specification is specific to the new JavaScript implementation of the Ruleset validator which is part of the 2021 Validator.
+
+A `JSON schema <https://github.com/IATI/IATI-Rulesets/blob/version-2.03/schema.json>`_ is availible to test that the structure of a Ruleset is correct.
+
+Each JSON document has the form.::
+
+    {
+        "CONTEXT": {
+            "RULE_NAME": {
+                "cases": CASE_DICT_ARRAY
+            }
+        }
+
+    }
+
+Where ``CONTEXT`` is an xpath expression. This will be used to select the XML elements that the contained rules will be tested against.
+``RULE_NAME`` is one of rule names listed below
+``CASE_DICT`` is a dictionary where the contents depend on ``RULE_NAME``
+``CASE_DICT_ARRAY`` is an array of case dictionaries. The contents of each dictionary depend on the ``RULE_NAME``
+
+The possible keys in a case dictionary are:
+
+``condition``
+    An xpath string. If this evaluates to False, the rule will be ignored.
+``idCondition``
+    If this evaluates to False, the rule will be ignored.
+    NOT_EXISTING_ORG_ID - Checks that values in ``paths`` are NOT an existing Publisher Organisation ID from the Registry
+    NOT_EXISTING_ORG_ID_PREFIX - Checks that values in ``paths`` are NOT prefixed with an existing Publisher Organisation ID from the Registry
+``eval``
+    An xpath string. Can evaluate to True or False.
+``paths``
+    An array of xpath strings. These are evaluated to give a list of elements that the named rule then operates upon.
+``less``
+    A string containing the xpath of the smaller value (or older value when working with dates).
+``more``
+    A string containing the xpath of the larger value (or more recent value when working with dates).
+``regex``
+    A string containing a perl style regular expression.
+``sum``
+    A number that is the expected sum.
+``excluded``
+    An array of xpath strings. Evaluate which elements should not coexist with other elements.
+``date``
+    A string containing the xpath to a date.
+``start``
+    A string containing the xpath to a start date.
+``end``
+    A string containing the xpath to an end date.
+``one``
+    A string containing the xpath of something that must exist or ``all`` must be followed.
+``all``
+    A string containing the condition that must be met for all elements if ``one`` is not met.
+``foreach``
+    An xpath string to group the loop by and evalute each ``do`` with the substituted result. 
+``do``
+    An array of rules. To evaluate with ``foreach``. Available rules include: ``strict_sum``, ``if_then``, ``atleast_one``, ``no_more_than_one`` 
+``subs``
+    An array of keys where the value matched in ``foreach`` should be substituded for the ``$1`` values in the ``do`` cases.
+``prefix``
+    An array of xpath strings. The matches of which, contain the possible prefixes for a path. If set to "ORG-ID-PREFIX" for startsWith, checks against provided list of prefixes in ``idSets.ORG-ID-PREFIX``. ``idSets`` are passed to the Rule evaluation code.
+
+Rule Names
+----------
+
+**Rule names are listed in bold**
+    Keys: The keys for each rule are then listed. The are listed in **snake-case**/**camel-case**.
+
+    Followed by a brief description of the rule's function.
+
+
+**no_more_than_one**/**noMoreThanOne**
+    Keys: ``condition``, ``paths``
+
+    There must be no more than one element described by the given paths.
+
+**atleast_one**/**atLeastOne**
+    Keys: ``condition``, ``paths``
+
+    There must be at least one element described by the given paths.
+
+**only_one_of**/**onlyOneOf**
+    Keys: ``excluded``, ``paths``
+
+    If there's a match of the elements in ``excluded``, there must not be any matches in ``paths``, if there are no matches in ``excluded``, there must be exactly one element from ``paths``.
+
+**one_or_all**/**oneOrAll**
+    Keys: ``one``, ``all``
+
+    ``one`` must exist otherwise ``all`` other attributes or elements must exist. 
+
+**dependent**/**NOT IMPLEMENTED IN JS**
+    Keys: ``condition``, ``paths``
+
+    If one of the provided paths exists, they must all exist.
+
+**sum**/**sum**
+    Keys: ``condition``, ``paths``, ``sum``
+
+    The numerical sum of the values of elements matched by ``paths`` must match the value for the ``sum`` key
+
+**date_order**/**dateOrder**
+    Keys: ``condition``, ``less``, ``more``
+
+    The date matched by ``less`` must not be after the date matched by ``more``. If they are equal, the are valid. If either of these dates is not found, the rule is ignored.
+    https://drive.google.com/file/d/1-R-xGMCrAKiadMBIHsNc4Xvl75CB0IV1/view
+    
+**date_now**/**dateNow**
+    Keys: ``date``
+
+    The ``date`` must not be after the current date.
+
+**time_limit**/**timeLimit**
+    Keys: ``start``, ``end``
+
+    The difference between the ``start`` date and the ``end`` date must not be greater than a year.
+
+**between_dates**/**betweenDates**
+    Keys: ``date``, ``start``, ``end``
+
+    The ``date`` must be between the ``start`` and ``end`` dates.
+
+**regex_matches**/**regexMatches**
+    Keys: ``condition``, ``idCondition``, ``paths``, ``regex``
+
+    The provided ``regex`` must match the text of all elements matched by ``paths``. ``idCondition`` is also an optional parameter.
+
+**regex_no_matches**/**regexNoMatches**
+    Keys: ``condition``, ``paths``, ``regex``
+
+    The provided ``regex`` must match the text of none of the elements matched by ``paths``.
+
+**startswith**/**startsWith**
+    Keys: ``condition``, ``idCondition``, ``paths``, ``start``, ``separator``
+
+    The text of each element matched by ``paths`` must start with the text of one of the elements matched by ``prefix`` (or a list of prefixed provided in ``idSets``) with an optional ``separator`` in between
+    ``prefix````separator````pathMatchText``. ``idCondition`` is also an optional parameter.
+
+**unique**/**unique**
+    Keys: ``condition``, ``paths``
+
+    The text of each of the elements described by ``paths`` must be unique
+
+**if_then**/**ifThen**
+    Keys: ``condition``, ``cases``, ``if``, ``then``, ``paths``
+
+    If the condition evaluated in ``if`` is true, then ``then`` must resolve to true as well
+    ``paths`` can be defined to provide additional context data in the output if a rule fails, but had no bearing on the pass/fail of the rule 
+
+**loop**/**loop**
+    Keys: ``foreach``, ``do``, ``cases``, ``subs``
+
+    All elements in ``foreach`` are evaluated under the rules inside ``do``
+
+**strict_sum**/**strictSum**
+    Keys: ``paths``, ``sum``
+
+    The decimal sum of the values of elements matched by ``paths`` must match the value for the ``sum`` key
+
+**no_spaces**/**noSpaces**
+    Keys: ``paths``
+
+    The text of each of the elements described by ``paths`` should not start or end with spaces or newlines 
--- iatirulesets/text.py
+++ iatirulesets/text.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
+import os
 import re
-import copy
 
 
 def human_list(other_paths, separator='or'):
@@ -18,102 +18,62 @@ def extract_from_expr(expr):
     return expr
 
 
-def rules_text(rules, reduced_path, show_all=False):
+def simplify_xpath(xpath):
+    return re.sub(r'\[[^\]]*\]', '', xpath)
+
+
+def rule_link(rule_id):
+    """Returns a Github link given a rule ID"""
+    default_url = 'https://github.com/IATI/IATI-Rulesets/blob/v2.03/validatorV2/rulesets/standard.json'
+    base_url = default_url + '#L'
+    file_dirname = os.path.dirname(__file__)
+    if 'IATI-Rulesets' in file_dirname:
+        rule_path = os.path.abspath(os.path.join(file_dirname, '../rulesets/standard.json'))
+    else:
+        rule_path = os.path.abspath(os.path.join(file_dirname, '../IATI-Rulesets/rulesets/standard.json'))
+    with open(rule_path) as standard_json:
+        for num, line in enumerate(standard_json, 1):
+            if '"' + rule_id + '"' in line:
+                return base_url + str(num)
+    return default_url
+
+
+def rules_text(rules, reduced_path=None):
+    possible_path_keys = ['less', 'more', 'start', 'end', 'date', 'one']
     out = []
     for rule in rules:
         cases = rules[rule]['cases']
         for case in cases:
-            simplify_xpath = lambda x: re.sub(r'\[[^\]]*\]', '', x)
-            if 'paths' in case:
-                for case_path in case['paths']:
-                    # Don't forget [@ ]
-                    if show_all or simplify_xpath(case_path) == reduced_path:
-                        other_paths = copy.deepcopy(case['paths'])
-                        other_paths.remove(case_path)
-                        if rule == 'only_one':
-                            out.append('``{0}`` must be present only once.'.format(case_path))
-                            if other_paths:
-                                out.append('``{0}`` must not be present if ``{1}`` are present.'.format(case_path, human_list(other_paths)))
-                                break
-                        elif rule == 'atleast_one':
-                            cond = case.get('condition', None)
-                            cond = '.' if not cond else ' if {}'.format(cond)
-
-                            if other_paths:
-                                out.append('Either ``{0}`` or ``{1}`` must be present{2}'.format(case_path, human_list(other_paths), cond))
-                                break
-                            else:
-                                out.append('``{0}`` must be present{1}'.format(case_path, cond))
-
-                        elif rule == 'only_one_of':
-                            out.append('``{0}`` must not be present alongisde ``{1}``.'.format(case_path, human_list(case['excluded'], 'and')))
-                        elif rule == 'startswith':
-                            out.append('``{0}`` should start with the value in ``{1}``'.format(case_path, case['start']))
-                        elif rule == 'regex_matches':
-                            out.append('``{0}`` should match the regex ``{1}``'.format(case_path, case['regex']))
-                        elif rule == 'no_more_than_one':
-                            if other_paths:
-                                out.append('There must be no more than one element or attribute matched at ``{0}`` or ``{1}``.'.format(case_path, human_list(other_paths)))
-                            else:
-                                out.append('There must be no more than one element or attribute matched at ``{0}``.'.format(case_path))
-                        elif rule == 'sum':
-                            sum_total = case['sum']
-                            if other_paths:
-                                out.append('The sum of values matched at ``{0}`` and ``{1}`` must be ``{2}``.'.format(case_path, human_list(other_paths, 'and'), sum_total))
-                                break
-                            else:
-                                out.append('The sum of values matched at ``{0}`` must be ``{2}``.'.format(case_path, sum_total))
-                        elif rule == 'strict_sum':
-                            out.append('The sum of values matched at ``{0}`` must be ``{1}``.'.format(case_path, case['sum']))
-                        elif rule == 'range':
-                            min_val = case.get('min')
-                            max_val = case.get('max')
-                            if min_val is not None or max_val is not None:
-                                # only proceed if one of the two exists
-                                txt = 'The value of each of the elements described by ``{0}`` must be'.format(case_path)
-                                txt += ' at least ``{0}``'.format(min_val) if min_val is not None else ''
-                                txt += ' no more than ``{0}``'.format(max_val) if max_val is not None else ''
-                                txt += ' (inclusive).'
-                                out.append(txt)
-                        else:
-                            print('Not implemented', rule, reduced_path)
-            else:
-                # rather than checking line-by-line wether reduced_path is in either one of the specific cases
-                # we do a generic check to assess we're rendering the right rule for the right element
-                if rule == 'date_order':
-                    if show_all or reduced_path == simplify_xpath(case['less']) or reduced_path == simplify_xpath(case['more']):
-                        if case['less'] == 'NOW':
-                            out.append('``{0}`` must not be in the past.'.format(case['more']))
-                        elif case['more'] == 'NOW':
-                            out.append('``{0}`` must not be in the future.'.format(case['less']))
-                        else:
-                            out.append('``{0}`` must be before or the same as ``{1}``'.format(case['less'], case['more']))
-
-                elif rule == 'time_limit':
-                    if show_all or reduced_path == simplify_xpath(case['start']) or reduced_path == simplify_xpath(case['end']):
-                        out.append('The time between ``{0}`` and ``{1}`` must not be over a year'.format(case['start'], case['end']))
-
-                elif rule == 'date_now':
-                    if show_all or reduced_path == simplify_xpath(case['date']):
-                        out.append('``{0}`` must not be more recent than the current date'.format(case['date']))
-
-                elif rule == 'if_then':
-                    if_case = extract_from_expr(case['if'])
-                    if show_all or (if_case.startswith(reduced_path) or if_case.endswith(reduced_path)):
-                        out.append('If ``{0}`` evaluates to true, then ``{1}`` must evaluate to true.'.format(case['if'], case['then']))
-
-                elif rule == 'one_or_all':
-                    if show_all or reduced_path == simplify_xpath(case['one']):
-                        out.append('``{0}`` must exist, otherwise all ``{1}`` must exist.'.format(case['one'], case['all']))
-
-                elif rule == 'between_dates':
-                    if show_all or reduced_path == simplify_xpath(case['date']):
-                        out.append('The ``{0}`` must be between the ``{1}`` and ``{2}`` dates.'.format(case['date'], case['start'], case['end']))
-
-                elif rule == 'loop':
-                    continue
-                #   commenting this out and skipping until the way we render this is fixed
-                #   out.append('All elements in ``{0}`` are evaluated under the rules inside ``{1}``.'.format(case['foreach'],case['do']))
+            if 'ruleInfo' in case:
+                if reduced_path:
+                    if 'paths' in case:
+                        for case_path in case['paths']:
+                            if simplify_xpath(case_path) == reduced_path:
+                                out.append((case['ruleInfo']['id'], case['ruleInfo']['message'], rule_link(case['ruleInfo']['id'])))
+                    included_path_keys = [path_key for path_key in possible_path_keys if path_key in case.keys()]
+                    for included_path_key in included_path_keys:
+                        case_path = case[included_path_key]
+                        if simplify_xpath(case_path) == reduced_path:
+                            out.append((case['ruleInfo']['id'], case['ruleInfo']['message'], rule_link(case['ruleInfo']['id'])))
                 else:
-                    print('Not implemented', rule, reduced_path)
-    return out
+                    out.append((case['ruleInfo']['id'], case['ruleInfo']['message'], rule_link(case['ruleInfo']['id'])))
+            else:
+                sub_rules = case['do'].keys()
+                for sub_rule in sub_rules:
+                    sub_cases = case['do'][sub_rule]['cases']
+                    for sub_case in sub_cases:
+                        if reduced_path:
+                            if 'paths' in sub_case:
+                                for sub_case_path in sub_case['paths']:
+                                    if simplify_xpath(sub_case_path) == reduced_path:
+                                        out.append((sub_case['ruleInfo']['id'], sub_case['ruleInfo']['message'], rule_link(sub_case['ruleInfo']['id'])))
+                            included_path_keys = [path_key for path_key in possible_path_keys if path_key in sub_case.keys()]
+                            for included_path_key in included_path_keys:
+                                case_path = sub_case[included_path_key]
+                                if simplify_xpath(case_path) == reduced_path:
+                                    out.append((sub_case['ruleInfo']['id'], sub_case['ruleInfo']['message'], rule_link(sub_case['ruleInfo']['id'])))
+                        else:
+                            out.append((sub_case['ruleInfo']['id'], sub_case['ruleInfo']['message'], rule_link(sub_case['ruleInfo']['id'])))
+    unique_rules = [tup for tup in (set(tuple(i) for i in out))]
+    unique_rules.sort(key=lambda x: list(map(int, x[0].split('.'))))
+    return unique_rules
